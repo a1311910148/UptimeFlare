6119a1cfc3ed52bd07809c80defe15a49998d523
a1311910148 <110203965+a1311910148@users.noreply.github.com>
2025-06-26 10:18:07 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -13,7 +13,7 @@ const pageConfig: PageConfig = {
   // If not specified, all monitors will be shown in a single list
   // If specified, monitors will be grouped and ordered, not-listed monitors will be invisble (but still monitored)
   group: {
-    '🌐 Public': ['baidu_monitor', 'akile_iij_ssh_monitor']
+    '🌐 Public': ['baidu_monitor', 'akile_iij_ssh_monitor','akile_iij_google_monitor']
   },
 }
 
